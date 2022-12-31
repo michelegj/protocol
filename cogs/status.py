182fdc404657e8b7e083d9063cf80dc0fc206952
Michele <55063409+michelegj@users.noreply.github.com>
2022-12-31 18:12:43 +0000

    string format

--- cogs/status.py
+++ cogs/status.py
@@ -13,8 +13,8 @@ class Ready(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def status(self, ctx, *message : str):
         if len(message) <= 15:
-            await ctx.send(f"`[protocol] changed status msg to: {message}`", delete_after=30)
-            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=message))
+            await ctx.send(f"`[protocol] changed status msg to: "+"{}".format(" ".join(message))+"`", delete_after=30)
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="{}".format(" ".join(message))))
         else:
             await ctx.send("`[protocol] limit of 15 chars exceeded!`", delete_after=12)
         await ctx.message.delete()
